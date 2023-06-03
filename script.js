be46958ec5fb4e41e0632a2fa826305b545779cc
zackdesu <zarchxxx@gmail.com>
2023-06-03 18:40:20 +0700

    finished (all bug i found)

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const scrollPos = window.scrollY;
 
-// const secAttr = document.querySelector("#attraction");
-// const scrSecAttr = secAttr.offsetTop;
-
 window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
+  const secAttr = document.querySelector("#attraction");
+  const scrSecAttr = secAttr.offsetTop;
   header.classList.toggle(
     "first",
-    window.scrollY > 2 && window.scrollY <= 1350
+    window.scrollY > 2 && window.scrollY <= scrSecAttr
   );
-  header.classList.toggle("second", window.scrollY >= 1350);
+  header.classList.toggle("second", window.scrollY >= scrSecAttr);
 
+  const under = document.querySelector(".undermountain");
+  const scrUnder = under.offsetTop;
   const underH1 = document.querySelector(".undermountain h1");
   const underP = document.querySelectorAll(".undermountain p");
 
-  if (window.scrollY > 199) {
+  if (window.scrollY > scrUnder - 400) {
     underH1.classList.add("to100");
     underP.forEach((e) => {
       e.classList.add("to100");
@@ -23,22 +24,21 @@ window.addEventListener("scroll", () => {
 
   const attH1 = document.querySelector("#attraction h1");
   const card = document.querySelectorAll(".card");
-  if (window.scrollY > 1000) {
+  if (window.scrollY > scrSecAttr - 400) {
     attH1.classList.add("to100");
     card.forEach((e) => {
       e.classList.add("to100");
     });
   }
 
+  const pla = document.querySelector("#place");
+  const scrPla = pla.offsetTop;
+
   const plaH1 = document.querySelector("#place h1");
   const plaIFrame = document.querySelector("#place iframe");
-  if (window.scrollY > 1500) {
+  if (window.scrollY > scrPla - 400) {
     plaH1.classList.add("to100");
     plaIFrame.classList.add("to100");
-  }
-
-  if (scrollPos > secAttr) {
-    console.log("hyu");
   }
 });
 
